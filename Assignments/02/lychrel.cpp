68781a8b3558061f1bfb7197d427d54a8d5322e0
Seth141 <seth4040@hotmail.com>
2023-10-11 21:54:51 -0700

    Much faster after multi-threading and dynamic scheduling were added! Will try to reduce time even more.

--- Assignments/02/lychrel.cpp
+++ Assignments/02/lychrel.cpp
@@ -1,6 +1,7 @@
 /////////////////////////////////////////////////////////////////////////////
 //
 //  lychrel.cpp
+//  (Seth Nuzum's version)
 //
 //  A program that searches for the largest (in terms of number of
 //    iterations without exceeding a specified maximum) palindrome value.
@@ -14,65 +15,48 @@
 #include <iomanip>
 #include <iostream>
 #include <vector>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 
 #include "LychrelData.h"
 
-// A structure recording the starting number, and its final palindrome.
-//   An vector of these records (typedefed to "Records") is built during
-//   program execution, and output before the program terminates.
 struct Record {
     Number n;
     Number palindrome;
 };
 using Records = std::vector<Record>;
 
-// Application specific constants
 const size_t MaxIterations = 7500;
 const size_t MaxThreads = 10;
 
-//
-// --- main ---
-//
-int main() {
-    LychrelData data;
+std::mutex dataMutex; 
+std::mutex recordsMutex;
+std::condition_variable condVar;
+size_t current_index = 0;
 
-    std::cerr << "Processing " << data.size() << " values ...\n";
+// This is a worker function that will be executed by each thread
+void worker(LychrelData& data, size_t& maxIter, Records& records) {
+    while (true) {
+        dataMutex.lock();
+        if (current_index >= data.size()) {
+            dataMutex.unlock();
+            return;
+        }
+        Number number = data[current_index++];
+        dataMutex.unlock();
 
-    size_t maxIter = 0;  // Records the current maximum number of iterations
-    Records records; // list of values that took maxIter iterations
-
-    // Iterate across all available data values, processing them using the 
-    //   reverse-digits and sum technique described in class.
-    for (auto i = 0; i < data.size(); ++i) {
-        Number number = data[i];
-        
         size_t iter = 0;
         Number n = number;
-
-        // The Lychrel loop - for any iteration, take the number, reverse
-        //   its digits, and sum those values together.  If that sum
-        //   is a palindrome, stop processing
+        
+        //While loop for palindrome verification:
         while (!n.is_palindrome() && ++iter < MaxIterations) {
-            Number sum(n.size());   // Value used to store current sum of digits
-            Number r = n.reverse(); // reverse the digits of the value
-
-            // An iterator pointing to the first digit of the reversed
-            //   value.  This iterator will be incremented to basically
-            //   traverse the digits of the main number in reverse
-            auto rd = n.begin(); 
+            Number sum(n.size());
+            Number r = n.reverse();
+            auto rd = n.begin();
             
-            bool carry = false;  // flag to indicate if we had a carry
-
-            // Sum the digits using the "transform" algorithm.  This
-            //   algorithm traverses a range of values (in our case,
-            //   starting with the least-siginificant [i.e., right most]
-            //   digit) of the original number, adding each digit to its
-            //   matching digit (by position) in the reversed number.
-            //
-            // The result is stored in the sum variable, which is
-            //   built up one digit at a time, respecting if a carry
-            //   digit was necessary for any iteration. 
-            std::transform(n.rbegin(), n.rend(), sum.rbegin(), 
+            bool carry = false;
+            std::transform(n.rbegin(), n.rend(), sum.rbegin(),
                 [&](auto d) {
                     auto v = d + *rd++ + carry;
     
@@ -82,41 +66,45 @@ int main() {
                     return v;
                 }
             );
-
-            // If there's a final carry value, prepend that to the sum
             if (carry) { sum.push_front(1); }
-
-            // Transfer the sum making it the next number to be processed
-            //   (i.e., reversed, summed, and checked if it's a
-            //   palindrome)
             n = sum;
         }
 
-        // Update our records.  First, determine if we have a new
-        //   maximum number of iterations that isn't the control limit
-        //   (MaxIterations) for a particular number.  If we're less
-        //   tha the current maximum (maxIter) or we've exceeded the number
-        //   of permissible iterations, ignore the current result and move
-        //   onto the next number.
-        if (iter < maxIter || iter == MaxIterations) { continue; }
+        recordsMutex.lock();
+        if (iter < maxIter || iter == MaxIterations) {
+            recordsMutex.unlock();
+            continue;
+        }
 
-        // Otherwise update our records, which possibly means discarding
-        //   our current maximum and rebuilding our records list.
         Record record{number, n};
         if (iter > maxIter) {
             records.clear();
             maxIter = iter;
         }
-
         records.push_back(record);
-    }
-
-    // Output our final results
-    std::cout << "\nmaximum number of iterations = " << maxIter << "\n";
-    for (auto& [number, palindrome] : records) {
-        std::cout 
-            << "\t" << number 
-            << " : [" << palindrome.size() << "] "
-            << palindrome << "\n";
+        // Unlocking dataMutex after accessing the shared data here: 
+        recordsMutex.unlock();
     }
 }
+
+int main() {
+    LychrelData data;
+    std::cerr << "Processing " << data.size() << " values ...\n";
+
+    size_t maxIter = 0;
+    Records records;
+
+    std::vector<std::thread> threads;
+    for (size_t i = 0; i < MaxThreads; ++i) {
+        threads.push_back(std::thread(worker, std::ref(data), std::ref(maxIter), std::ref(records)));
+    }
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
+    std::cout << "\nmax num of iterations = " << maxIter << "\n";
+    for (auto& [number, palindrome] : records) {
+        std::cout << "\t" << number << " : [" << palindrome.size() << "] " << palindrome << "\n";
+    }
+}
+
